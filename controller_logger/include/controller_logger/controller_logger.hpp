d5dd42bbadb0eb649b3fc4f00039cd07ab6976c5
adriano <adrscbl@gmail.com>
2025-04-23 18:26:28 +0200

    building - not working with joint trajectory

--- controller_logger/include/controller_logger/controller_logger.hpp
+++ controller_logger/include/controller_logger/controller_logger.hpp
@@ -4,30 +4,55 @@
 
 #include "controller_interface/chainable_controller_interface.hpp"
 #include "controller_logger/controller_logger_parameters.hpp"
+#include <rclcpp/publisher.hpp>
 #include <std_msgs/msg/float64_multi_array.hpp>
 #include <realtime_tools/realtime_publisher.hpp>
+#include <realtime_tools/realtime_buffer.hpp>
 #include <rclcpp_lifecycle/node_interfaces/lifecycle_node_interface.hpp>
 #include <sensor_msgs/msg/joint_state.hpp>
 
 
 namespace controller_logger
 {
+  using DataType = std_msgs::msg::Float64MultiArray;
   class ControllerLogger : public controller_interface::ChainableControllerInterface
   {
   public:
     controller_interface::InterfaceConfiguration command_interface_configuration() const override;
+
     controller_interface::InterfaceConfiguration state_interface_configuration() const override;
+
     controller_interface::return_type update_and_write_commands(
       const rclcpp::Time & time, const rclcpp::Duration & period) override;
+
     controller_interface::CallbackReturn on_init() override;
+
+    controller_interface::CallbackReturn on_configure(
+      const rclcpp_lifecycle::State & previous_state) override;
+
+    controller_interface::CallbackReturn on_activate(
+      const rclcpp_lifecycle::State & previous_state) override;
+
+    controller_interface::CallbackReturn on_deactivate(
+      const rclcpp_lifecycle::State & previous_state) override;
+
+    bool on_set_chained_mode(bool chained_mode) override;
 
   protected:
     std::vector<hardware_interface::CommandInterface> on_export_reference_interfaces() override;
-    controller_interface::return_type update_reference_from_subscribers(const rclcpp::Time & time, const rclcpp::Duration & period) override;
 
-    rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr logger_publisher_;
+    controller_interface::return_type update_reference_from_subscribers(
+      const rclcpp::Time & time, const rclcpp::Duration & period) override;
+
     std::shared_ptr<ParamListener> param_listener_;
+    Params params_;
     bool read_from_interface_;
+    realtime_tools::RealtimeBuffer<std::shared_ptr<DataType>> rt_buffer_ptr_;
+    rclcpp::Subscription<DataType>::SharedPtr joints_cmd_sub_;
+    rclcpp::Publisher<DataType>::SharedPtr logger_pub_;
+    std::vector<std::string> reference_interface_names_;
+    std::vector<std::string> command_interface_names_;
+    std::vector<std::string> joint_names_;
   };
 } // namespace controller_logger
 
--- controller_logger/src/controller_logger.cpp
+++ controller_logger/src/controller_logger.cpp
@@ -1,35 +1,148 @@
 #include "controller_logger/controller_logger.hpp"
+#include <controller_interface/controller_interface_base.hpp>
 #include <pluginlib/class_list_macros.hpp>
+#include "controller_interface/helpers.hpp"
+
 
 namespace controller_logger
 {
+
 controller_interface::CallbackReturn ControllerLogger::on_init()
 {
-  param_listener_ = std::make_shared<ParamListener>(get_node());
-  logger_publisher_ = get_node()->create_publisher<sensor_msgs::msg::JointState>("~/logged_commands", 10);
+  try
+  {
+    param_listener_ = std::make_shared<ParamListener>(get_node());
+    params_ = param_listener_->get_params();
+  }
+  catch (const std::exception & e)
+  {
+    fprintf(stderr, "Exception thrown during init stage with message: %s \n", e.what());
+    return controller_interface::CallbackReturn::ERROR;
+  }
+  return controller_interface::CallbackReturn::SUCCESS;
+}
 
-  get_node()->get_parameter("read_from_interface", read_from_interface_);
+controller_interface::CallbackReturn ControllerLogger::on_configure(
+  const rclcpp_lifecycle::State & /*previous_state*/)
+{
+    logger_pub_ = get_node()->create_publisher<DataType>("~/logged_commands", 10);
+    params_ = param_listener_->get_params();
+    command_interface_names_ = params_.interfaces;
+  
+    // pre-reserve command interfaces
+    command_interfaces_.reserve(command_interface_names_.size());
+  
+    RCLCPP_INFO(this->get_node()->get_logger(), "configure successful");
+  
+    // The names should be in the same order as for command interfaces for easier matching
+    reference_interface_names_ = command_interface_names_;
+    // for any case make reference interfaces size of command interfaces
+    reference_interfaces_.resize(
+      reference_interface_names_.size(), std::numeric_limits<double>::quiet_NaN());
+  
+    return controller_interface::CallbackReturn::SUCCESS;
+}
+
+controller_interface::CallbackReturn ControllerLogger::on_activate(
+  const rclcpp_lifecycle::State & /*previous_state*/)
+{
+  //  check if we have all resources defined in the "points" parameter
+  //  also verify that we *only* have the resources defined in the "points" parameter
+  // ATTENTION(destogl): Shouldn't we use ordered interface all the time?
+  std::vector<std::reference_wrapper<hardware_interface::LoanedCommandInterface>>
+    ordered_interfaces;
+  if (
+    !controller_interface::get_ordered_interfaces(
+      command_interfaces_, command_interface_names_, std::string(""), ordered_interfaces) ||
+    command_interface_names_.size() != ordered_interfaces.size())
+  {
+    RCLCPP_ERROR(
+      this->get_node()->get_logger(), "Expected %zu command interfaces, got %zu",
+      command_interface_names_.size(), ordered_interfaces.size());
+    return controller_interface::CallbackReturn::ERROR;
+  }
+
+  // reset command buffer if a command came through callback when controller was inactive
+  rt_buffer_ptr_ = realtime_tools::RealtimeBuffer<std::shared_ptr<DataType>>(nullptr);
+
+  RCLCPP_INFO(this->get_node()->get_logger(), "activate successful");
+
+  std::fill(
+    reference_interfaces_.begin(), reference_interfaces_.end(),
+    std::numeric_limits<double>::quiet_NaN());
 
   return controller_interface::CallbackReturn::SUCCESS;
 }
 
+controller_interface::CallbackReturn ControllerLogger::on_deactivate(
+  const rclcpp_lifecycle::State & /*previous_state*/)
+{
+  // reset command buffer
+  rt_buffer_ptr_ = realtime_tools::RealtimeBuffer<std::shared_ptr<DataType>>(nullptr);
+  return controller_interface::CallbackReturn::SUCCESS;
+}
+
+bool ControllerLogger::on_set_chained_mode(bool /*chained_mode*/) { return true; }
+
 controller_interface::InterfaceConfiguration ControllerLogger::command_interface_configuration() const
 {
-  auto state_interfaces_config_names = std::vector<std::string>{"dummy_interface"};
-  return {
-    controller_interface::interface_configuration_type::INDIVIDUAL, state_interfaces_config_names};
+  return controller_interface::InterfaceConfiguration{
+    controller_interface::interface_configuration_type::NONE};
 }
 
-controller_interface::InterfaceConfiguration ControllerLogger::state_interface_configuration() const
+controller_interface::InterfaceConfiguration ControllerLogger::state_interface_configuration()
+const
 {
-  auto state_interfaces_config_names = std::vector<std::string>{"dummy_interface"};
-  return {
-    controller_interface::interface_configuration_type::INDIVIDUAL, state_interfaces_config_names};
+  return controller_interface::InterfaceConfiguration{
+    controller_interface::interface_configuration_type::NONE};
 }
 
+controller_interface::return_type ControllerLogger::update_and_write_commands(
+  const rclcpp::Time & time, const rclcpp::Duration & period)
+{
+  std_msgs::msg::Float64MultiArray msg;
+  msg.data.reserve(command_interfaces_.size());
+  for (const auto & interface : command_interfaces_)
+  {
+    msg.data.push_back(interface.get_value());
+  }
+  logger_pub_->publish(msg);
 
-controller_interface::return_type update_and_write_commands(
-  const rclcpp::Time & time, const rclcpp::Duration & period) {
+  return controller_interface::return_type::OK;
+}
+
+std::vector<hardware_interface::CommandInterface>
+ControllerLogger::on_export_reference_interfaces()
+{
+  std::vector<hardware_interface::CommandInterface> reference_interfaces;
+
+  for (size_t i = 0; i < reference_interface_names_.size(); ++i)
+  {
+    reference_interfaces.push_back(
+      hardware_interface::CommandInterface(
+        get_node()->get_name(), reference_interface_names_[i], &reference_interfaces_[i]));
+  }
+
+  return reference_interfaces;
+}
+
+controller_interface::return_type ControllerLogger::update_reference_from_subscribers(
+  const rclcpp::Time & /*time*/, const rclcpp::Duration & /*period*/)
+{
+  auto joint_commands = rt_buffer_ptr_.readFromRT();
+  // message is valid
+  if (!(!joint_commands || !(*joint_commands)))
+  {
+    if (reference_interfaces_.size() != (*joint_commands)->data.size())
+    {
+      RCLCPP_ERROR_THROTTLE(
+        get_node()->get_logger(), *(get_node()->get_clock()), 1000,
+        "command size (%zu) does not match number of reference interfaces (%zu)",
+        (*joint_commands)->data.size(), reference_interfaces_.size());
+      return controller_interface::return_type::ERROR;
+    }
+    reference_interfaces_ = (*joint_commands)->data;
+  }
 
   return controller_interface::return_type::OK;
 }
