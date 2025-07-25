ce0e749ca2c879e70114d7849fd4c025bee2a8f2
adrianoscibilia <adrscbl@gmail.com>
2025-07-25 15:34:14 +0200

    avevo modificato la classe in due sottoclassi per gestire lo stop per staccare i servizi ROS. Però poi mi sono accorto che la classe moo era inutile. Avrei quindi potuto non fare nulla

--- rehab_gui/rehab_gui/async_ros_events.py
+++ rehab_gui/rehab_gui/async_ros_events.py
@@ -19,14 +19,26 @@ from ethercat_controller_msgs.action import SetModesOfOperationAction
 
 from action_msgs.msg import GoalStatus
 
-class ASyncRosManager(QObject):
+def get_status_string(status):
+    status_dict = {
+        GoalStatus.STATUS_UNKNOWN: 'UNKNOWN',
+        GoalStatus.STATUS_ACCEPTED: 'ACCEPTED',
+        GoalStatus.STATUS_EXECUTING: 'EXECUTING',
+        GoalStatus.STATUS_CANCELING: 'CANCELING',
+        GoalStatus.STATUS_SUCCEEDED: 'SUCCEEDED',
+        GoalStatus.STATUS_CANCELED: 'CANCELED',
+        GoalStatus.STATUS_ABORTED: 'ABORTED'
+    }
+    return status_dict.get(status, 'UNKNOWN')
+
+class FollowJointTrajectoryActionManager(QObject):
     finished = pyqtSignal()
-    moo_finished = pyqtSignal()
     progress = pyqtSignal(int)
-    
-    def __init__(self, ros_node, joint_names, controller_name='joint_trajectory_controller'):
-        super(ASyncRosManager, self).__init__()
-        self._node = ros_node
+    stopped = pyqtSignal()
+
+    def __init__(self, ros_node : Node, joint_names : list, controller_name='joint_trajectory_controller'):
+        super().__init__() # type: ignore
+        self.node = ros_node
         self._init_time_s = 0
         self._is_paused = False
         self._pause_start_time = None
@@ -34,23 +46,19 @@ class ASyncRosManager(QObject):
         self._joint_names = joint_names
         #   action client
         self.controller_name = controller_name
-        self.clientFollowCartTraj = ActionClient(self._node, FollowJointTrajectory, f'/{self.controller_name}/follow_joint_trajectory')
-        if not self.clientFollowCartTraj.wait_for_server(2):
-            print("Follow Joint Trajectory Action server not available, exiting...")
-            rclpy.shutdown()
-            sys.exit(1)
-        self.clientSetMOOAsync = ActionClient(self._node, SetModesOfOperationAction, '/state_controller/async_set_modes_of_operation')
-        if not self.clientSetMOOAsync.wait_for_server(2):
-            print("Set Modes of Operation Action server not available, exiting...")
-        self.clearFCT() 
+        self.client = ActionClient(self.node, FollowJointTrajectory, f'/{self.controller_name}/follow_joint_trajectory')
+        if not self.client.wait_for_server(2):
+            raise ValueError('Follow Joint Trajectory Action server not available, exiting...')
+        self._goal_handle = None
+        self.clear()
 
-    def clearFCT(self):
+    def clear(self):
         self.goalFCT = FollowJointTrajectory.Goal()
         self.goalFCT.trajectory.joint_names = self._joint_names
 
-    def setFCT(self, cartesian_positions, time_from_start,**kwargs):
+    def set(self, cartesian_positions, time_from_start,**kwargs):
         # Called only at the beginning
-        self.clearFCT()
+        self.clear()
         if type(time_from_start[0]) is not Duration: 
             self._total_time_s = (time_from_start[-1][0] - time_from_start[0][0])
         else:
@@ -80,9 +88,9 @@ class ASyncRosManager(QObject):
             acc = np.array([s.derivative(2)(t_iPoint) for s in splines])
             self._time_from_start_duration[iPoint] = Duration(sec=int(t_iPoint),nanosec=int((t_iPoint - int(t_iPoint)) * 1e9))
             self._point_velocities[iPoint] = vel
-            self.add_pointFCT(pos, vel, acc, t_iPoint, **kwargs)
+            self.addPoint(pos, vel, acc, t_iPoint, **kwargs)
 
-    def setSpeedOverrideFCT(self, speed_ovr):
+    def setSpeedOverride(self, speed_ovr : int = 100):
         self.speed_scale = speed_ovr / 100.0
         vel_scaled = [v * self.speed_scale for v in self._point_velocities]
         acc_scaled = [a * self.speed_scale*self.speed_scale for a in self._point_velocities]
@@ -110,7 +118,7 @@ class ASyncRosManager(QObject):
                         else: 
                             print(f"Warning: Acceleration at point {iPoint} is not zero, but it should be. Acc: {vel_scaled[iPoint]}")
 
-    def add_pointFCT(self, positions, velocities, accelerations, time, **kwargs):
+    def addPoint(self, positions, velocities, accelerations, time, **kwargs):
         point=JointTrajectoryPoint()
         point.positions = positions
         point.velocities = velocities
@@ -118,49 +126,50 @@ class ASyncRosManager(QObject):
         point.effort = [] * len(positions)
         point.time_from_start = time
         self.goalFCT.trajectory.points.append(point)
-         
-    def start_movementFCT(self, default_speed_ovr):
-        # Called at the beginning of each movement
-        self.setSpeedOverrideFCT(default_speed_ovr)
+        
+    def startMovement(self, default_speed_ovr):
+    # Called at the beginning of each movement
+        self.setSpeedOverride(default_speed_ovr)
         self._init_time_s = time.time()
         self._paused_duration = 0.0
         self._last_actual_time_pct = 0.0
         self._pause_start_time = None
-        self.goalFCT.trajectory.header.stamp = self._node.get_clock().now().to_msg()
-        self._send_goal_future = self.clientFollowCartTraj.send_goal_async(self.goalFCT)
-        self._send_goal_future.add_done_callback(self.goal_response_callback)
+        self.goalFCT.trajectory.header.stamp = self.node.get_clock().now().to_msg()
+        self._send_goal_future = self.client.send_goal_async(self.goalFCT)
+        self._send_goal_future.add_done_callback(self.on_goal_accepted)
 
     def is_paused(self, trigger_pause):
         if trigger_pause:
             if not self._is_paused:
                 self._pause_start_time = time.time()
             self._is_paused = True
-            self._node.get_logger().info("⏸️ Paused movement")
+            self.node.get_logger().info("⏸️ Paused movement")
         else:
             if self._is_paused and self._pause_start_time is not None:
                 pause_duration = time.time() - self._pause_start_time
                 self._paused_duration += pause_duration
-                self._node.get_logger().info(f"▶️ Resumed movement after {pause_duration:.2f}s pause")
+                self.node.get_logger().info(f"▶️ Resumed movement after {pause_duration:.2f}s pause")
                 self._pause_start_time = None
             self._is_paused = False
 
-    def goal_response_callback(self, future):
+    def on_goal_accepted(self, future):
         self._goal_handle = future.result()
         if not self._goal_handle.accepted:
-            self._node.get_logger().info('Goal rejected!!')
+            self.node.get_logger().info('Goal rejected!!')
             return
-        self._node.get_logger().info('Goal accepted')
+        self.node.get_logger().info('Goal accepted')
         self._get_goal_result_future = self._goal_handle.get_result_async()
-        self._get_goal_result_future.add_done_callback(self._done_callback)
+        self._get_goal_result_future.add_done_callback(self.on_done)
         self._goal_status = GoalStatus.STATUS_UNKNOWN
-        self.timer = self._node.create_timer(0.02, lambda: self.check_status())
+        self.timer = self.node.create_timer(0.02, lambda: self.check_status())
 
-    def _done_callback(self, future):
+    def on_done(self, future):
         try:
             self.finished.emit()
             self.timer.cancel()
+            self._goal_handle = None
         except Exception as e:
-            self._node.get_logger().error(f'Exception in _done_callback: {e}')
+            self.node.get_logger().error(f'Exception in on_done: {e}')
 
     def check_status(self):
         if self._is_paused:
@@ -170,9 +179,9 @@ class ASyncRosManager(QObject):
         old_status = self._goal_status
         self._goal_status = self._goal_handle.status
         if old_status != self._goal_status:
-            old_status_string = self.get_status_string(old_status)
-            status_string = self.get_status_string(self._goal_status)
-            self._node.get_logger().info(f'Action transition from {old_status_string} to {status_string}')
+            old_status_string = get_status_string(old_status)
+            status_string = get_status_string(self._goal_status)
+            self.node.get_logger().info(f'Action transition from {old_status_string} to {status_string}')
 
         actual_time = time.time()
         effective_time = actual_time - self._init_time_s - self._paused_duration
@@ -182,42 +191,84 @@ class ASyncRosManager(QObject):
 
         self.progress.emit(int(actual_time_from_start_percentage))
         self._last_actual_time_pct = int(actual_time_from_start_percentage)
-      
-    @staticmethod
-    def get_status_string(status):
-        status_dict = {
-            GoalStatus.STATUS_UNKNOWN: 'UNKNOWN',
-            GoalStatus.STATUS_ACCEPTED: 'ACCEPTED',
-            GoalStatus.STATUS_EXECUTING: 'EXECUTING',
-            GoalStatus.STATUS_CANCELING: 'CANCELING',
-            GoalStatus.STATUS_SUCCEEDED: 'SUCCEEDED',
-            GoalStatus.STATUS_CANCELED: 'CANCELED',
-            GoalStatus.STATUS_ABORTED: 'ABORTED'
-        }
-        return status_dict.get(status, 'UNKNOWN')
 
-    def stopFCT(self):
-        if hasattr(self, '_goal_handle'):
-            cancel_future = self._goal_handle.cancel_goal_async()
-            self.clearFCT()
+    def on_cancelled(self, future):
+        if future.result() is not None:
+            self.node.get_logger().info(f'Goal cancelled: {future.result()}')
         else:
-            self._node.get_logger().info('goal handle has not been created yet')
+            self.node.get_logger().info('Goal cancelled without result')
+        self.clear()
+        self.stopped.emit()
+        self.goal_handle = None
+    
+    def stop(self):
+        if hasattr(self, '_goal_handle') and self._goal_handle is not None:
+            cancel_future = self._goal_handle.cancel_goal_async()
+            cancel_future.add_done_callback(self.on_cancelled)
+        else:
+            self.node.get_logger().info('goal handle has not been created yet')
 
-    def change_mode_of_op(self, dof, new_mode):
-        self.mooGoal = SetModesOfOperationAction.Goal()
-        self.mooGoal.dof_name = dof
-        self.mooGoal.mode_of_operation = new_mode
-        self.moo_future = self.clientSetMOOAsync.send_goal_async(self.mooGoal)
-        self.moo_future.add_done_callback(self.accepted_mode_of_op)
 
-    def accepted_mode_of_op(self, future):
-        self.moo_goal_handle = future.result()
-        if not self.moo_goal_handle.accepted:
-            self._node.get_logger().info('Goal rejected!!')
+class ModesOfOperationActionManager(QObject):
+    finished    = pyqtSignal()
+    stopped    = pyqtSignal()
+    def __init__(self, ros_node : Node):
+        super().__init__() # type: ignore
+        self.node = ros_node
+        self.goal_handle = None
+        self.client = ActionClient(ros_node, SetModesOfOperationAction, '/state_controller/async_set_modes_of_operation')
+        if not self.client.wait_for_server(2):
+            raise ValueError('Set Modes of Operation Action server not available, exiting...')
+    
+    def switch_mode_of_operation(self, dof : str, new_mode: str):
+        self.goal = SetModesOfOperationAction.Goal()
+        self.goal.dof_name = dof
+        self.goal.mode_of_operation = new_mode
+        self.moo_future = self.client.send_goal_async(self.goal)
+        self.moo_future.add_done_callback(self.on_goal_accepted)
+
+    def on_goal_accepted(self, future):
+        self.goal_handle = future.result()
+        if not self.goal_handle.accepted:
+            self.node.get_logger().info('Goal rejected!!')
             return
-        self._node.get_logger().info('Goal accepted')
-        self._get_moo_goal_result_future = self.moo_goal_handle.get_result_async()
-        self._get_moo_goal_result_future.add_done_callback(self.moo_done_callback)
+        self.node.get_logger().info('Goal accepted')
+        self._get_moo_goal_result_future = self.goal_handle.get_result_async()
+        self._get_moo_goal_result_future.add_done_callback(self.on_done)
 
-    def moo_done_callback(self, future):
-        self.moo_finished.emit()
+    def on_done(self, future):
+        self.finished.emit()
+        self.goal_handle = None
+
+    def on_cancelled(self, future):
+        if future.result() is not None:
+            self.node.get_logger().info(f'Goal cancelled: {future.result().message}')
+        else:
+            self.node.get_logger().info('Goal cancelled without result')
+        self.stopped.emit()
+        self.goal_handle = None
+
+    def stop(self):
+        if hasattr(self, 'goal_handle') and self.goal_handle is not None:
+            cancel_future = self.goal_handle.cancel_goal_async()
+            cancel_future.add_done_callback(self.on_cancelled)
+            self.goal = None
+    
+
+class ASyncRosManager(QObject):
+
+    def __init__(self, ros_node, joint_names, controller_name='joint_trajectory_controller'):
+        super(ASyncRosManager, self).__init__()
+        
+        self._fct = FollowJointTrajectoryActionManager(ros_node, joint_names, controller_name)
+        self._moo = ModesOfOperationActionManager(ros_node)
+        
+    def moo(self) -> ModesOfOperationActionManager:
+        return self._moo
+    
+    def fct(self) -> FollowJointTrajectoryActionManager:
+        return self._fct
+
+    def stop(self):
+        self._fct.stop()
+        self._moo.stop()
