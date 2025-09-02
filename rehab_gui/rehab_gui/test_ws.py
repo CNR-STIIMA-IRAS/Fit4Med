d3207685c0c55f3792b7a218bac7e13f2ece475e
adrianoscibilia <adrscbl@gmail.com>
2025-09-02 16:27:07 +0200

    ws - wip

--- rehab_gui/rehab_gui/test_ws.py
+++ rehab_gui/rehab_gui/test_ws.py
@@ -5,7 +5,6 @@ import roslibpy
 client = roslibpy.Ros(host='10.2.16.42', port=9090)
 client.run()
 print('Is ROS connected?', client.is_connected)
-client.terminate()
 
 listener = roslibpy.Topic(client, '/joint_states', 'sensor_msgs/msg/JointState')
 listener.subscribe(lambda message: print('Heard talking: ' + message['data']))
