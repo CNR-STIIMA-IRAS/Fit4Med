e8273c075b839f3df44f8f6c5f992b8578ccb965
adrianoscibilia <adrscbl@gmail.com>
2025-09-02 16:46:33 +0200

    ws - wip

--- rehab_gui/rehab_gui/test_ws.py
+++ rehab_gui/rehab_gui/test_ws.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import time
 import roslibpy
+import roslibpy.actionlib
 
 client = roslibpy.Ros(host='10.2.16.42', port=9090)
 client.run()
@@ -9,9 +10,31 @@ print('Is ROS connected?', client.is_connected)
 listener = roslibpy.Topic(client, '/PLC_controller/plc_states', 'plc_controller_msgs/msg/PlcController')
 listener.subscribe(lambda message: print(f'Heard talking: {message}'))
 
-try:
-    while True:
-        pass
-except KeyboardInterrupt:
-    client.terminate()
-client.terminate()
+
+action_client = roslibpy.actionlib.ActionClient(client,
+                                                '/joint_trajectory_controller/follow_joint_trajectory',
+                                                'control_msgs/action/FollowJointTrajectory')
+
+goal = {'trajectory': {'joint_names': ['joint_x', 'joint_y', 'joint_z'],
+            'points': [{'positions': [0.0, 0.0, 0.0],
+                        'velocities': [0.0, 0.0, 0.0],
+                        'accelerations': [0.0, 0.0, 0.0],
+                        'time_from_start': {'secs': 2, 'nsecs': 0}},
+                       {'positions': [0.1, 0.1, 0.1],
+                        'velocities': [0.0, 0.0, 0.0],
+                        'accelerations': [0.0, 0.0, 0.0],
+                        'time_from_start': {'secs': 4, 'nsecs': 0}},
+                       {'positions': [0.2, 0.2, 0.2],
+                        'velocities': [0.0, 0.0, 0.0],
+                        'accelerations': [0.0, 0.0, 0.0],
+                        'time_from_start': {'secs': 6, 'nsecs': 0}}]}}
+
+goal = roslibpy.actionlib.Goal(action_client,
+                               roslibpy.Message(goal))
+
+goal.on('feedback', lambda f: print(f))
+goal.send()
+result = goal.wait(10)
+action_client.dispose()
+
+print(f'Result: {result}')
