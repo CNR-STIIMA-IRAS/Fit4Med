d6466d7f739a3862758d97014db479cc040d6201
adrianoscibilia <adrscbl@gmail.com>
2025-09-02 16:17:39 +0200

    ws - wip

--- rehab_gui/rehab_gui/test_ws.py
+++ rehab_gui/rehab_gui/test_ws.py
@@ -1,6 +1,6 @@
 import roslibpy
 
-client = roslibpy.Ros(host='localhost', port=9090)
+client = roslibpy.Ros(host='10.2.16.42', port=9090)
 client.run()
 print('Is ROS connected?', client.is_connected)
 client.terminate()
