3cf18ba2e2357c99e5f70f52b57d4ac40b9a637d
Nicola Pedrocchi <nicola.pedrocchi@cnr.it>
2026-06-29 09:47:16 +0200

    wired in the GUI the values of the commanded PLC output

--- ps_scripts/fmrr_gui.ps1
+++ ps_scripts/fmrr_gui.ps1
@@ -1,5 +1,23 @@
 # Copyright 2026 CNR-STIIMA
 # SPDX-License-Identifier: Apache-2.0
 
-# Execute the Python script remotely
-python "C:\Fit4Med\rehab_gui\rehab_gui\FMRRMainProgram.py" --remote-ip 192.168.1.1 --maximise-window
+$ErrorActionPreference = "Stop"
+
+try {
+    python "C:\Fit4Med\rehab_gui\rehab_gui\FMRRMainProgram.py" --remote-ip 192.168.1.1 --maximise-window
+    $exitCode = $LASTEXITCODE
+
+    if ($exitCode -ne 0) {
+        Write-Host ""
+        Write-Host "GUI exited with error code $exitCode" -ForegroundColor Red
+        Read-Host "Press Enter to close"
+        exit $exitCode
+    }
+}
+catch {
+    Write-Host ""
+    Write-Host "Failed to launch GUI:" -ForegroundColor Red
+    Write-Host $_
+    Read-Host "Press Enter to close"
+    exit 1
+}
